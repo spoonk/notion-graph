f73e01c6812cf953f7868ddcf32f813d911f30b2
spoonk <cornwlucas@gmail.com>
2025-03-05 07:03:59 -0800

    middleware

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,5 @@
 module server
 
 go 1.24.1
+
+require github.com/lmittmann/tint v1.0.7 // indirect
--- server/server.go
+++ server/server.go
@@ -2,9 +2,42 @@ package main
 
 import (
 	"fmt"
+	"github.com/lmittmann/tint"
+	"log/slog"
 	"net/http"
+	"os"
+	"time"
 )
 
+func welcome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to my website!")
+}
+
+// middleware pattern:
+//
+//	wrap a route call in a chain of middleware functions
+//	route fn called after all middleware fns called
+func logging(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Debug(r.URL.Path)
+		f(w, r)
+	}
+}
+
 func main() {
-	fmt.Println("hiiii")
+	initLogging()
+
+	slog.Info("Starting server on port 8080")
+	http.HandleFunc("/", logging(welcome))
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func initLogging() {
+	slog.SetDefault(slog.New(
+		tint.NewHandler(os.Stderr, &tint.Options{
+			Level:      slog.LevelDebug,
+			TimeFormat: time.Kitchen,
+		}),
+	))
 }
